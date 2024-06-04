606b6745c48d0281d21c9b8de70da1b0c988b898
Luc35b <baraya.lucas@ufl.edu>
2024-06-04 08:53:23 -0400

    added animation

--- karibs/lib/main.dart
+++ karibs/lib/main.dart
@@ -12,7 +12,6 @@ const Color LightPurple = Color(0xFFEFDAF9);
 const Color NotWhite = Color(0xFFEFEBF1);
 const Color White = Colors.white;
 
-
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await DatabaseHelper().deleteDatabaseFile(); // Delete the existing database, remove when done testing
@@ -25,7 +24,6 @@ void main() async {
     ),
   );
 }
-
 
 class MyApp extends StatelessWidget {
   @override
@@ -40,7 +38,31 @@ class MyApp extends StatelessWidget {
   }
 }
 
-class MainScreen extends StatelessWidget {
+class MainScreen extends StatefulWidget {
+  @override
+  _MainScreenState createState() => _MainScreenState();
+}
+
+class _MainScreenState extends State<MainScreen> {
+  bool _isAnimating = false;
+
+  void _navigateToScreen(Widget screen) {
+    setState(() {
+      _isAnimating = true;
+    });
+
+    Future.delayed(Duration(milliseconds: 500), () {
+      Navigator.push(
+        context,
+        MaterialPageRoute(builder: (context) => screen),
+      ).then((_) {
+        setState(() {
+          _isAnimating = false;
+        });
+      });
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -48,96 +70,112 @@ class MainScreen extends StatelessWidget {
         title: Text(''),
         backgroundColor: DeepPurple,
       ),
-      body: Container(
-        decoration: BoxDecoration(
-            color: White
-        ),
-        child: Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              SizedBox(height: 25),
-              Image.asset('images/logo.jpg'),
-              Text('WELCOME TO', style: GoogleFonts.raleway(fontSize: 34, color: DeepPurple)),
-              Expanded(
-                child: Text('KLAS', style: GoogleFonts.raleway(fontSize: 50, fontWeight: FontWeight.bold, color: DeepPurple)),
-              ),
-              Container(
-                padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 35), // Padding inside the container
-                decoration: BoxDecoration(
-                  color: DeepPurple,
-                  border: Border.all(width: 2, color: DeepPurple),
-                  borderRadius: BorderRadius.only(
-                    topLeft: Radius.circular(30),
-                    topRight: Radius.circular(5),
-                    bottomLeft: Radius.circular(5),
-                    bottomRight: Radius.circular(30),
+      body: Stack(
+        children: [
+          AnimatedContainer(
+            duration: Duration(milliseconds: 500),
+            curve: Curves.easeInOut,
+            transform: Matrix4.translationValues(0, _isAnimating ? 600 : 0, 0),
+            child: AnimatedOpacity(
+              opacity: _isAnimating ? 0.0 : 1.0,
+              duration: Duration(milliseconds: 500),
+              child: Container(
+                decoration: BoxDecoration(color: White),
+                child: Center(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      SizedBox(height: 25),
+                      Image.asset('images/logo.jpg'),
+                      Text(
+                        'WELCOME TO',
+                        style: GoogleFonts.raleway(fontSize: 34, color: DeepPurple),
+                      ),
+                      Expanded(
+                        child: Text(
+                          'KLAS',
+                          style: GoogleFonts.raleway(fontSize: 50, fontWeight: FontWeight.bold, color: DeepPurple),
+                        ),
+                      ),
+                      Container(
+                        padding: EdgeInsets.symmetric(vertical: 35.0, horizontal: 35), // Padding inside the container
+                        decoration: BoxDecoration(
+                          color: DeepPurple,
+                          border: Border.all(width: 2, color: DeepPurple),
+                          borderRadius: BorderRadius.only(
+                            topLeft: Radius.circular(30),
+                            topRight: Radius.circular(5),
+                            bottomLeft: Radius.circular(5),
+                            bottomRight: Radius.circular(30),
+                          ),
+                          boxShadow: [
+                            BoxShadow(
+                              color: Colors.black12,
+                              blurRadius: 10,
+                              offset: Offset(3, 3), // Shadow position
+                            ),
+                          ],
+                        ),
+                        child: Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              'CHOOSE USER TYPE',
+                              style: GoogleFonts.raleway(
+                                fontSize: 28,
+                                fontWeight: FontWeight.bold,
+                                color: White,
+                              ),
+                            ),
+                            SizedBox(height: 24),
+                            Column(
+                              children: [
+                                ElevatedButton(
+                                  onPressed: () => _navigateToScreen(TeacherDashboard()),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: White,
+                                    foregroundColor: Colors.white,
+                                    side: BorderSide(width: 2, color: MidPurple),
+                                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 18), // Button padding
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'I\'m a Teacher',
+                                    style: GoogleFonts.raleway(fontSize: 28, color: DeepPurple),
+                                  ),
+                                ),
+                                SizedBox(height: 25),
+                                ElevatedButton(
+                                  onPressed: () => _navigateToScreen(StudentScreen()),
+                                  style: ElevatedButton.styleFrom(
+                                    backgroundColor: White,
+                                    foregroundColor: Colors.white,
+                                    side: BorderSide(width: 2, color: MidPurple),
+                                    padding: EdgeInsets.symmetric(horizontal: 60, vertical: 18), // Button padding
+                                    shape: RoundedRectangleBorder(
+                                      borderRadius: BorderRadius.circular(5),
+                                    ),
+                                  ),
+                                  child: Text(
+                                    'I\'m a Student',
+                                    style: GoogleFonts.raleway(fontSize: 28, color: DeepPurple),
+                                  ),
+                                ),
+                              ],
+                            ),
+                          ],
+                        ),
+                      ),
+                      SizedBox(height: 10),
+                    ],
                   ),
-
-                  //borderRadius: BorderRadius.circular(30), // Rounded corners for all
-                  boxShadow: [
-                    BoxShadow(
-                      color: Colors.black12,
-                      blurRadius: 10,
-                      offset: Offset(3, 3), // Shadow position
-                    ),
-                  ],
-                ),
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Text('CHOOSE USER TYPE', style: GoogleFonts.raleway(fontSize: 28, fontWeight: FontWeight.bold, color: White)),
-                    SizedBox(height: 24),
-                    Column(
-                      children:[
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => TeacherDashboard()),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: White,
-                            foregroundColor: Colors.white,
-                            side: BorderSide(width: 2, color: MidPurple),
-                            padding: EdgeInsets.symmetric(horizontal: 60, vertical: 18), // Button padding
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          child: Text('I\'m a Teacher', style: GoogleFonts.raleway(fontSize: 28, color: DeepPurple)),
-                        ),
-                        SizedBox(height: 25),
-                        ElevatedButton(
-                          onPressed: () {
-                            Navigator.push(
-                              context,
-                              MaterialPageRoute(builder: (context) => StudentScreen()),
-                            );
-                          },
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: White,
-                            foregroundColor: Colors.white,
-                            side: BorderSide(width: 2, color: MidPurple),
-                            padding: EdgeInsets.symmetric(horizontal: 60, vertical: 18), // Button padding
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(5),
-                            ),
-                          ),
-                          child: Text('I\'m a Student', style: GoogleFonts.raleway(fontSize: 28, color: DeepPurple)),
-                        ),
-                      ],
-                    ),
-
-                  ],
                 ),
               ),
-              SizedBox(height: 10),
-
-            ],
+            ),
           ),
-        ),
+        ],
       ),
     );
   }
