0c10db0c5a216000f8dd0b6ac16a99a5693cf953
Aryaan Verma <verma.aryaan@gmail.com>
2024-06-24 15:40:41 +0000

    added functionality to add students when there are none for a class

--- karibs/lib/screens/test_grade_screen.dart
+++ karibs/lib/screens/test_grade_screen.dart
@@ -211,108 +211,118 @@ class _TestGradeScreenState extends State<TestGradeScreen> {
       appBar: AppBar(
         title: Text('Grade Exam for ${widget.testTitle}'),
       ),
-      body: _isLoading
-          ? const Center(child: CircularProgressIndicator())
-          : Center(
-        child: Column(
-          children: [
-            if (_className != null)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Grading Exam: ${widget.testTitle} for "$_className" ',
-                  style: const TextStyle(fontSize: 20),
-                  textAlign: TextAlign.center,
-                ),
-              ),
-            Padding(
-              padding: const EdgeInsets.all(8.0),
-              child: DropdownButton<int>(
-                hint: const Text("Select Student"),
-                value: _selectedStudentId,
-                onChanged: (int? newValue) {
-                  setState(() {
-                    _selectedStudentId = newValue;
-                    _initializeCategoryScores();
-                    _initializeQuestionCorrectness();
-                    question_answer_map.clear();
-                  });
-                },
-                items: _students.map<DropdownMenuItem<int>>((Map<String, dynamic> student) {
-                  return DropdownMenuItem<int>(
-                    value: student['id'],
-                    enabled: !_gradedStudentIds.contains(student['id']),
-                    child: Text(
-                      student['name'],
-                      style: TextStyle(
-                        color: _gradedStudentIds.contains(student['id']) ? Colors.grey : Colors.black,
-                      ),
-                    ),
-                  );
-                }).toList(),
-              ),
+        body: _isLoading
+            ? const Center(child: CircularProgressIndicator())
+            : Center(
+          child: _students.isEmpty
+              ? Center(
+            child: ElevatedButton(
+              onPressed: () {
+                _goToTeacherDashboard(widget.classId);
+              },
+              child: const Text('Go to Class to Add Students'),
             ),
-            if (_selectedStudentId != null)
+          )
+              : Column(
+            children: [
+              if (_className != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Grading Exam: ${widget.testTitle} for "$_className" ',
+                    style: const TextStyle(fontSize: 20),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
               Padding(
                 padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  'Selected Student: ${_students.firstWhere((student) => student['id'] == _selectedStudentId)['name']}',
-                  style: const TextStyle(fontSize: 18),
-                ),
-              ),
-            if (_selectedStudentId != null)
-              Expanded(
-                child: ListView.builder(
-                  itemCount: _questions.length,
-                  itemBuilder: (context, index) {
-                    int questionId = _questions[index]['id'];
-                    int categoryId = _questions[index]['category_id'];
-                    String categoryName =
-                    _categories.firstWhere((category) => category['id'] == categoryId)['name'];
-                    return ListTile(
-                      title: Text(_questions[index]['text']),
-                      subtitle: Text(categoryName),
-                      tileColor: questionCorrectness[questionId] == 1
-                          ? Colors.green.withOpacity(0.2)
-                          : questionCorrectness[questionId] == -1
-                          ? Colors.red.withOpacity(0.2)
-                          : questionCorrectness[questionId] == 0
-                          ? Colors.grey.withOpacity(0.2)
-                          : null,
-                      trailing: Row(
-                        mainAxisSize: MainAxisSize.min,
-                        children: [
-                          IconButton(
-                            icon: const Icon(Icons.check, color: Colors.green),
-                            onPressed: () {
-                              _markCorrect(questionId, categoryId);
-                            },
-                          ),
-                          IconButton(
-                            icon: const Icon(Icons.clear, color: Colors.red),
-                            onPressed: () {
-                              _markIncorrect(questionId, categoryId);
-                            },
-                          ),
-                        ],
+                child: DropdownButton<int>(
+                  hint: const Text("Select Student"),
+                  value: _selectedStudentId,
+                  onChanged: (int? newValue) {
+                    setState(() {
+                      _selectedStudentId = newValue;
+                      _initializeCategoryScores();
+                      _initializeQuestionCorrectness();
+                      question_answer_map.clear();
+                    });
+                  },
+                  items: _students.map<DropdownMenuItem<int>>((Map<String, dynamic> student) {
+                    return DropdownMenuItem<int>(
+                      value: student['id'],
+                      enabled: !_gradedStudentIds.contains(student['id']),
+                      child: Text(
+                        student['name'],
+                        style: TextStyle(
+                          color: _gradedStudentIds.contains(student['id']) ? Colors.grey : Colors.black,
+                        ),
                       ),
                     );
-                  },
+                  }).toList(),
                 ),
               ),
-            if (_gradedStudentIds.length == _students.length)
-              Padding(
-                padding: const EdgeInsets.all(8.0),
-                child: Text(
-                  "View student report to edit their score.",
-                  style: TextStyle(fontSize: 18, color: Colors.grey),
-                  textAlign: TextAlign.center,
+              if (_selectedStudentId != null)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    'Selected Student: ${_students.firstWhere((student) => student['id'] == _selectedStudentId)['name']}',
+                    style: const TextStyle(fontSize: 18),
+                  ),
                 ),
-              ),
-          ],
+              if (_selectedStudentId != null)
+                Expanded(
+                  child: ListView.builder(
+                    itemCount: _questions.length,
+                    itemBuilder: (context, index) {
+                      int questionId = _questions[index]['id'];
+                      int categoryId = _questions[index]['category_id'];
+                      String categoryName =
+                      _categories.firstWhere((category) => category['id'] == categoryId)['name'];
+                      return ListTile(
+                        title: Text(_questions[index]['text']),
+                        subtitle: Text(categoryName),
+                        tileColor: questionCorrectness[questionId] == 1
+                            ? Colors.green.withOpacity(0.2)
+                            : questionCorrectness[questionId] == -1
+                            ? Colors.red.withOpacity(0.2)
+                            : questionCorrectness[questionId] == 0
+                            ? Colors.grey.withOpacity(0.2)
+                            : null,
+                        trailing: Row(
+                          mainAxisSize: MainAxisSize.min,
+                          children: [
+                            IconButton(
+                              icon: const Icon(Icons.check, color: Colors.green),
+                              onPressed: () {
+                                _markCorrect(questionId, categoryId);
+                              },
+                            ),
+                            IconButton(
+                              icon: const Icon(Icons.clear, color: Colors.red),
+                              onPressed: () {
+                                _markIncorrect(questionId, categoryId);
+                              },
+                            ),
+                          ],
+                        ),
+                      );
+                    },
+                  ),
+                ),
+              if (_gradedStudentIds.length == _students.length)
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    "View student report to edit their score.",
+                    style: TextStyle(fontSize: 18, color: Colors.grey),
+                    textAlign: TextAlign.center,
+                  ),
+                ),
+            ],
+          ),
         ),
-      ),
-      bottomNavigationBar: BottomAppBar(
+      bottomNavigationBar: _students.isNotEmpty
+          ? BottomAppBar(
         child: SingleChildScrollView(
           scrollDirection: Axis.horizontal,
           child: Padding(
@@ -345,7 +355,7 @@ class _TestGradeScreenState extends State<TestGradeScreen> {
             ),
           ),
         ),
-      ),
+      ): null,
     );
   }
 }
